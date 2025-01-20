39c9fb533169b863eef720dc253ca04a35b66f3b
Psychea13 <kea260209@gmail.com>
2025-01-21 00:49:34 +0300

    module_19_2

--- djangoProject/task1/models.py
+++ djangoProject/task1/models.py
@@ -21,3 +21,21 @@ class Game(models.Model):
     def __str__(self):
         return self.title
 
+
+# QuerySet запросы:
+# python manage.py shell
+# from task1.models import Buyer, Game
+
+# Buyer.objects.create(name='Anne', balance=500, age=16)
+# Buyer.objects.create(name='Harry', balance=1000, age=21)
+# Buyer.objects.create(name='Bob', balance=700, age=40)
+# Buyer.objects.all()
+
+# Game.objects.create(title='Subnautica 2', cost=3000, size=20, description='Описание 1', age_limited = False)
+# Game.objects.create(title='S.T.A.L.K.E.R. 2', cost=5000, size=150, description='Описание 2', age_limited = True)
+# Game.objects.create(title='Atomic Heart', cost=4500, size=90, description='Описание 3', age_limited = True)
+# Game.objects.all()
+
+# Game.objects.get(id=4).buyer.set((Buyer.objects.get(id=1), Buyer.objects.get(id=3)))
+# Game.objects.get(id=5).buyer.set((Buyer.objects.get(id=2), Buyer.objects.get(id=3)))
+# Game.objects.get(id=6).buyer.set((Buyer.objects.get(id=2), Buyer.objects.get(id=3)))
